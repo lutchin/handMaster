f0b373c7a3e1ee7c1025787da81ba5b9fe1f12e6
YalovikEA <yalovikea@gmail.com>
2019-03-27 17:02:46 +0300

    styles for checkboxes

--- src/js/main.js
+++ src/js/main.js
@@ -84,6 +84,12 @@ $(document).ready(function() {
     }
   })
 
+  // Filter dropdown
+
+  $('.filter-category__item-title').on('click', function() {
+    $(this).parent().toggleClass('active');
+  })
+
 
   // Filter price range
   var stepsSlider = document.querySelector('.filter-price__line');
@@ -94,15 +100,85 @@ $(document).ready(function() {
   noUiSlider.create(stepsSlider, {
     start: [20, 80],
     connect: true,
-    tooltips: [true, wNumb({ decimals: 1 })],
+    format: wNumb({
+      decimals: 2,
+      mark: '.',
+      thousand: ' ',
+    }),
     range: {
       'min': [0],
-      'max': 200
+      '10%': [10, 1],
+      '50%': [80, 5],
+      '80%': 150,
+      'max': 2000
     }
   });
 
   stepsSlider.noUiSlider.on('update', function(values, handle) {
     inputs[handle].value = values[handle];
+  });
+
+  // Listen to keydown events on the input field.
+  inputs.forEach(function(input, handle) {
+
+    input.addEventListener('change', function() {
+      stepsSlider.noUiSlider.setHandle(handle, this.value);
+    });
+
+    input.addEventListener('keydown', function(e) {
+
+      var values = stepsSlider.noUiSlider.get();
+      var value = Number(values[handle]);
+
+      // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+      var steps = stepsSlider.noUiSlider.steps();
+
+      // [down, up]
+      var step = steps[handle];
+
+      var position;
+
+      // 13 is enter,
+      // 38 is key up,
+      // 40 is key down.
+      switch (e.which) {
+
+        case 13:
+          stepsSlider.noUiSlider.setHandle(handle, this.value);
+          break;
+
+        case 38:
+
+          // Get step to go increase slider value (up)
+          position = step[1];
+
+          // false = no step is set
+          if (position === false) {
+            position = 1;
+          }
+
+          // null = edge of slider
+          if (position !== null) {
+            stepsSlider.noUiSlider.setHandle(handle, value + position);
+          }
+
+          break;
+
+        case 40:
+
+          position = step[0];
+
+          if (position === false) {
+            position = 1;
+          }
+
+          if (position !== null) {
+            stepsSlider.noUiSlider.setHandle(handle, value - position);
+          }
+
+          break;
+      }
+    });
   });
 
 });
